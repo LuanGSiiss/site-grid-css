834871981ce1dcd60eb751f2dd036431a36b7d9c
Luan Gerber Siiss <luangerber666@gmail.com>
2025-09-01 21:33:02 -0300

    ajustes finais

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
-const CACHE_NAME = "gerber-cache-v1";
-const urlsToCache = [
+const CACHE_NAME = "pwa-chace-v1";
+const FILES_TO_CACHE = [
     "/",
     "/index.html",
+    "/manifest.json",
     "/styles.css",
-    "/assets/logo-svg.png"
+    "/assets/logo-svg.png",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png"
 ];
 
 // Instala o service worker
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
 });
 
 // Intercepta requisições
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request);
